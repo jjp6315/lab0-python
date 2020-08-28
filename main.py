e652ec74ef083c6810ffe34ee2de754421d11e6c
jjp6315 <jjp6315@psu.edu>
2020-08-28 02:27:44 +0000

    yes

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-print("Hello World!")
 name = input("Enter your name: ")
 
 print("Hello, " + name + "!")
@@ -6,4 +5,4 @@ age = input("Enter your age: ")
 age = int(age)
 
 
-print(f"You will be {age+1} next year")
+print(f"You will be {age+1} next year.")
